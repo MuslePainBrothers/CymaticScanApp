3b7753f00cf200da1919aa0839704e6c44ca3f2f
chestermx <chestermx88@gmail.com>
2017-02-05 14:49:58 +0900

    [add]htmlファイルの表示

--- CymaticScanApp/CymaticScanApp/settings.py
+++ CymaticScanApp/CymaticScanApp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "general"
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'CymaticScanApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
--- CymaticScanApp/general/views.py
+++ CymaticScanApp/general/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.views import generic
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the general index.")
+class IndexView(generic.TemplateView):
+    template_name = 'index.html'
