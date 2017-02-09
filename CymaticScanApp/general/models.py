e2e0fbf5b494bed5512ac1537e43fd53e93c45dd
atsuo1203 <atsuo@cps.im.dendai.ac.jp>
2017-02-10 02:07:10 +0900

    model追加

--- CymaticScanApp/general/models.py
+++ CymaticScanApp/general/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=30)
+    question = models.TextField(max_length=200)
