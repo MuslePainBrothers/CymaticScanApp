4a6ec6f0391be65ba612d178bf0df3fa53757623
atsuo1203 <atsuo@cps.im.dendai.ac.jp>
2017-02-10 02:11:53 +0900

    [add]ques_listらhtml追加

--- CymaticScanApp/general/views.py
+++ CymaticScanApp/general/views.py
@@ -2,8 +2,17 @@ from django.views import generic
 
 
 class TopView(generic.TemplateView):
-    template_name = 'top.html'
+    template_name = "top.html"
 
 
 class QuestionView(generic.TemplateView):
-    template_name = 'question.html'
+    template_name = "question.html"
+
+
+class ShowQuesListView(generic.TemplateView):
+    template_name = "ques_list.html"
+
+
+class ResultView(generic.TemplateView):
+    template_name = "result.html"
+
