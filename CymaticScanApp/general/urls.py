8a5cecad51b1d5ac94279c9d7697a606e210e7f5
chestermx <chestermx88@gmail.com>
2017-02-12 23:16:16 +0900

    [refact]urls.pyにおけるresultの正規表現削除

--- CymaticScanApp/general/urls.py
+++ CymaticScanApp/general/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     url(r'^$', views.TopView.as_view(), name='top'),
     url(r'^question/$', views.QuestionView.as_view(), name='question'),
     url(r'^ques_list/$', views.ShowQuesListView.as_view(), name='ques_list'),
-    url(r'^result/(?P<pk>[0-9]+)/$', views.ResultView.as_view(), name='result'),
+    url(r'^result/$', views.ResultView.as_view(), name='result'),
 ]
