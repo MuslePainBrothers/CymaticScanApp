408f1b410cee940f31d781b4f9062d968c630b2c
chestermx <chestermx88@gmail.com>
2017-02-14 22:28:28 +0900

    [update]トータルスコアと診断結果テキストの表示

--- CymaticScanApp/general/views.py
+++ CymaticScanApp/general/views.py
@@ -27,15 +27,37 @@ class ResultView(generic.TemplateView):
         for i in range(0, len(ques)):
             list_your_answer.append(Answer.objects.get(pk=self.request.POST["answer"+str(i+1)]))
 
+        total_score = [0, 0, 0, 0]
+        score_name = ["キチ度", "アスペ度", "池沼度", "狂気度"]
+
         # パラメータ計算（サンプル）
         for ans in list_your_answer:
-            print(ans, ans.para_A, ans.para_B, ans.para_C)
+            print(ans, ans.para_crazy, ans.para_aspect, ans.para_intdis, ans.para_madness)
+            total_score[0] += ans.para_crazy
+            total_score[1] += ans.para_aspect
+            total_score[2] += ans.para_intdis
+            total_score[3] += ans.para_madness
 
         # デバッグ用
         ques_your_answer = zip(ques, list_your_answer)
+        score_name_total_score = zip(score_name, total_score)
+
+        # 結果のテキスト
+        if total_score[0] > 4:
+            result_text = "軽度のキチ"
+        elif total_score[1] > 5:
+            result_text = "軽度のアスペ"
+        elif total_score[2] > 3:
+            result_text = "どこにでもいる池沼"
+        elif total_score[3] > 6:
+            result_text = "かなりやばいやつ"
+        else:
+            result_text = "ふつうの人"
 
         context = self.get_context_data(**kwargs)
         context["list_your_answer"] = list_your_answer
         context["ques_your_answer"] = ques_your_answer
+        context["score_name_total_score"] = score_name_total_score
+        context["result_text"] = result_text
 
         return self.render_to_response(context)
