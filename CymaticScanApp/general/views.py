ccef0205678fdf6b41bb4ea98cc2178583630fd7
chestermx <chestermx88@gmail.com>
2017-02-14 21:01:41 +0900

    [update]質問画面から結果画面へのパラメータデータの受け渡し実装

--- CymaticScanApp/general/views.py
+++ CymaticScanApp/general/views.py
@@ -1,4 +1,5 @@
 from django.views import generic
+from .models import Question, Answer
 
 
 class TopView(generic.TemplateView):
@@ -8,7 +9,33 @@ class TopView(generic.TemplateView):
 class QuestionView(generic.TemplateView):
     template_name = "question.html"
 
+    def get_context_data(self, *args, **kwargs):
+        context = super(QuestionView, self).get_context_data(**kwargs)
+        context["questions"] = Question.objects.all()
+        context["count"] = 0
+        return context
+
 
 class ResultView(generic.TemplateView):
     template_name = "result.html"
 
+    def post(self, request, *args, **kwargs):
+        ques = Question.objects.order_by("id")
+
+        # ユーザの解答
+        list_your_answer = []
+        for i in range(0, len(ques)):
+            list_your_answer.append(Answer.objects.get(pk=self.request.POST["answer"+str(i+1)]))
+
+        # パラメータ計算（サンプル）
+        for ans in list_your_answer:
+            print(ans, ans.para_A, ans.para_B, ans.para_C)
+
+        # デバッグ用
+        ques_your_answer = zip(ques, list_your_answer)
+
+        context = self.get_context_data(**kwargs)
+        context["list_your_answer"] = list_your_answer
+        context["ques_your_answer"] = ques_your_answer
+
+        return self.render_to_response(context)
